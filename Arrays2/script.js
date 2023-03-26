5b5ac87b03c5f71a30a26eae53e91727b34738e8
João Soares <joao.soares@cwi.com.br>
2023-03-26 00:01:38 -0300

    Array2_1

--- Arrays2/script.js
+++ Arrays2/script.js
@@ -13,16 +13,18 @@ const comments = [
   { text: "Nice Nice Nice!", id: 542328 },
 ];
 
-const isAdult = people.some((person) => {
-  new Date().getFullYear() - person.year >= 18;
-});
+const currentYear = new Date().getFullYear();
 
-const allAdults = people.every((person) => {
-  new Date().getFullYear() - person.year >= 18;
-});
+//Checks if at least one person in the people array is an adult
+const isAdult = people.some((person) => currentYear - person.year >= 18);
 
-const comment = comments.find((comment) => {
-  comment.id === 2039842;
-});
+// Checks if all people in the people array are adults
+const allAdults = people.every((person) => currentYear - person.year >= 18);
 
+//Finds the first comment in the comments array with a specific id
+const comment = comments.find((comment) => comment.id === 2039842);
+
+//Finds the index of the first comment in the comments array with a specific id
 const index = comments.findIndex((comment) => comment.id === 123523);
+
+console.log({ isAdult, allAdults, comment, index });
